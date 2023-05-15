72231d1a3b187d8c00a3ddaaa745aa0e2e28c1b4
newberg85 <bergviana12345@gmail.com>
2023-05-15 01:12:58 -0300

    primeira página

---
+++ js/script.js
@@ -0,0 +1,66 @@
+const fromText = document.querySelector(".text_from"),
+totext = document.querySelector(".to_from"),
+selectTag = document.querySelectorAll("select"),
+change = document.querySelector(".change_position"),
+translateBtn = document.getElementById("btn-translate"),
+icons = document.querySelectorAll(".row i");
+
+selectTag.forEach((tag, id) => {
+    for (const country_code in countries) {
+        let selected;
+        if(id == 0 && country_code == "en-GB") {
+            selected = "selected";
+        } else if(id == 1 && country_code == "pt-PT") {
+            selected = "selected";
+        }
+        let option = `<option class="opt_style" value="${country_code}" ${selected}>${countries[country_code]}</option>`;
+        tag.insertAdjacentHTML("beforeend", option);
+    }
+});
+
+change.addEventListener("click", () => {
+    let tempText = fromText.value,
+    tempLang = selectTag[0].value;
+    fromText.value = totext.value;
+    selectTag[0].value = selectTag[1].value;
+    totext.value = tempText ;
+    selectTag[1].value = tempLang ;
+
+});
+
+
+
+fromText.addEventListener("keypress", () => {
+    let text = fromText.value,
+        translateFrom = selectTag[0].value,
+        translateTo = selectTag[1].value;
+    let apiUrl = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`;
+    fetch(apiUrl)
+        .then(res => res.json())
+        .then(data => {
+            totext.value = data.responseData.translatedText;
+        });
+});
+
+icons.forEach(icon => {
+    icon.addEventListener("click", ({target}) =>{
+        if(target.classList.contains("fa-copy")) {
+            if(target.id =="from") {
+                navigator.clipboard.writeText(fromText.value);
+                alert("texto copiado")
+            } else {
+                navigator.clipboard.writeText(totext.value);
+            }
+        } else {
+            let utterance;
+            if(target.id =="from") {
+                utterance = new SpeechSynthesisUtterance(fromText.value)
+                utterance.lang = selectTag[0].value;
+            } else {
+                utterance = new SpeechSynthesisUtterance(totext.value)
+                utterance.lang = selectTag[1].value;
+            }
+            speechSynthesis.speak(utterance);
+        }
+    });
+});
